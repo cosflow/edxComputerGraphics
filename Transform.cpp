4788b3440444bd2db481aeefce3d5fc688517497
Andrés Cosme Flores <148059241+cosflow@users.noreply.github.com>
2024-10-14 19:14:48 +0200

    Add files via upload

---
+++ Transform.cpp
@@ -0,0 +1,53 @@
+// Transform.cpp: implementation of the Transform class.
+
+#include "Transform.h"
+
+//Please implement the following functions:
+
+// Helper rotation function.  
+mat3 Transform::rotate(const float degrees, const vec3& axis) {
+	float rads =glm::radians(degrees);
+	float x = axis.x;
+	float y = axis.y;
+	float z = axis.z;
+
+	glm::mat3 m1(1.0f);
+	glm::mat3 m2(x * x, x * y, x * z, y * x, y * y, y * z, z * x, z * y, z * z);
+	glm::mat3 m3(0, z, -y, -z, 0, x, y, -x, 0);
+
+	glm::mat3 ret = cos(rads)*m1 + (1-cos(rads))*m2 + sin(rads)*m3 ;
+	
+  return ret;
+}
+
+// Transforms the camera left around the "crystal ball" interface
+void Transform::left(float degrees, vec3& eye, vec3& up) {
+	mat3 rotation_matrix = Transform::rotate(degrees, up);
+	mat3 normal_rotation_matrix = glm::transpose(glm::inverse(rotation_matrix));
+	//float eye_length = glm::length(eye);
+	eye = rotation_matrix * eye;
+	up = normal_rotation_matrix * up;
+}
+
+// Transforms the camera up around the "crystal ball" interface
+void Transform::up(float degrees, vec3& eye, vec3& up) {
+	
+}
+
+// Your implementation of the glm::lookAt matrix
+mat4 Transform::lookAt(vec3 eye, vec3 up) {
+  // YOUR CODE FOR HW1 HERE
+
+  // You will change this return call
+  return mat4();
+}
+
+Transform::Transform()
+{
+
+}
+
+Transform::~Transform()
+{
+
+}
